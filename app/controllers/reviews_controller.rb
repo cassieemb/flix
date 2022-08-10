a82d74d3a3816d63d78865ef14e4555157f7abfd
Cassie <bowles.cassiee@gmail.com>
2022-08-10 11:49:56 -0500

    added edit, update, and destroy - error partial still isn't showing on failed validations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,30 +1,56 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
 
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new(review_params)
 
     if @review.save!
       redirect_to movie_reviews_path(@movie), notice: "Thanks for your review!"
     else
-      render :new
+      render :new, status: 422
+    end
+  end
+
+  def edit
+    @review = @movie.reviews.find(params[:id])
+  end
+
+  def update
+    @review = @movie.reviews.find(params[:id])
+
+    if @review.update(review_params)
+      redirect_to movie_reviews_path(@movie), notice: "Review successfully updated!"
+    else
+      render :edit, status: 422
+    end
+  end
+
+  def destroy
+    @review = @movie.reviews.find(params[:id])
+    if @review.destroy
+      redirect_to movie_reviews_path(@movie), alert: "Review successfully deleted!"
+    else
+      redirect_to movie_reviews_path(@movie)
     end
   end
 
 private
 
-def review_params
-  params.require(:review).
-    permit(:name, :comment, :stars)
-end
+  def review_params
+    params.require(:review).
+      permit(:name, :comment, :stars)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 
 end
