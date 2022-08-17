e00a9086dc41a88ee7bb814294152ef05577f800
Cassie <bowles.cassiee@gmail.com>
2022-08-17 12:16:19 -0500

    added user sign in and sign out functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,16 +15,39 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user), notice: "User account created successfully"
     else
       render :new, status: 422
     end
   end
 
+  def edit
+    @user = User.find_by(params[:id])
+  end
+
+  def update
+    @user = User.find_by(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "User account updated successfully"
+    else
+      render :edit, status: 422
+    end
+  end
+
+  def destroy
+    @user = User.find_by(params[:id])
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to movies_url, alert: "User account deleted successfully"
+
+  end
+
   private
 
   def user_params
     params.require(:user).
-      permit(:name, :email, :password, :password_confirmation)
+      permit(:name, :username, :email, :password, :password_confirmation)
   end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: {with: /\S+@\S+/}, uniqueness: { case_sensitive: false}
   validates :password, length: {minimum: 10, allow_blank: true}
+  validates :username, presence: true, uniqueness: { case_sensitive: false}, format: {with: /\A[A-Z0-9]+\z/i}
 end
